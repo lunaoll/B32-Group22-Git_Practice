af9342cf9679364affab5cc455f6df62ac3d2b9c
lunaoll <luna@cydeo.com>
2024-01-31 22:15:02 +0300

    add test case for login fucntionality

--- src/main/java/luna/week2/Question1.java
+++ src/main/java/luna/week2/Question1.java
@@ -4,5 +4,8 @@ public class Question1 {
 
     public static void main(String[] args) {
         System.out.println("test case 1 added here");
+
+
+        System.out.println("second test case implemnted");
     }
 }
